a14b940dabae76e5ba6b327a2bf070d011853b02
CarlosGT1999 <68020606+CarlosGT1999@users.noreply.github.com>
2021-09-14 16:12:11 -0500

    5to cambio

--- js/efect_equipos_toogle.js
+++ js/efect_equipos_toogle.js
@@ -1,34 +1,105 @@
 // botones
-var btn_claroVideo = $('#btn_claroVideo');
-var btn_claroApp = $('#btn_claroApp');;
+var btn_app = $('#1');
+var btn_video = $('#2');
+var btn_gaming = $('#3');
+var btn_proteccion = $('#4');
+
 // sliders
-var sliderClaroVideo = $('#slider_claroVideo,#slider_claroVideoImg');
-var sliderClaroApp = $('#slider_claroApp,#slider_claroAppImg');
+var sliderClaroApp = $('#slider_claroApp');
+var sliderClaroVideo = $('#slider_claroVideo');
+var sliderClaroGaming = $('#slider_claroGaming');
+var sliderClaroProteccion = $('#slider_claroProteccion');
+
+var btn_clickeado = 1;
 
 
-    btn_claroVideo.click(function(){
+    btn_app.click(function(){
   
-      btn_claroApp.css('color','#79838A');
-      btn_claroVideo.css('color','white');
-      sliderClaroApp.hide(10);
-      sliderClaroVideo.show(1000);
-      setTimeout(()=>
-      {
-          btn_claroVideo.click();
-      },9000)
-    
+        btn_app.css('color','white');
+        $('#'+btn_clickeado).css('color','#79838A');
+        btn_clickeado=1;
+        
+        sliderClaroApp.show();
+        sliderClaroVideo.hide();
+        sliderClaroGaming.hide();
+        sliderClaroProteccion.hide();
+        clickAutomatico();
+      
     });
 
-    btn_claroApp.click(function(){
+    btn_video.click(function(){
   
-        btn_claroApp.css('color','white');
-        btn_claroVideo.css('color','#79838A');
-        sliderClaroApp.show(1000);
-        sliderClaroVideo.hide(10);
       
-      });
+      btn_video.css('color','white');
+      $('#'+btn_clickeado).css('color','#79838A');
+      btn_clickeado=2;
+    
+      sliderClaroVideo.show();
+      sliderClaroApp.hide();
+      sliderClaroGaming.hide();
+      sliderClaroProteccion.hide();
+      clickAutomatico();
+      
+      
+    });
+
+
+    btn_gaming.click(function(){
+  
+      btn_gaming.css('color','white');
+      $('#'+btn_clickeado).css('color','#79838A');
+      btn_clickeado=3;
+
+      sliderClaroApp.hide();
+      sliderClaroVideo.hide();
+      sliderClaroProteccion.hide();
+      sliderClaroGaming.show();
+      clickAutomatico();
+      
+    });
+
+    btn_proteccion.click(function(){
+  
+      btn_proteccion.css('color','white');
+      $('#'+btn_clickeado).css('color','#79838A');
+      btn_clickeado=4;
+
+      sliderClaroApp.hide();
+      sliderClaroVideo.hide();
+      sliderClaroGaming.hide();
+      sliderClaroProteccion.show();
+      clickAutomatico();
+      btn_clickeado = 0;
+      setTimeout(()=>
+      {
+      
+        btn_proteccion.css('color','#79838A');
+        
+        
+      },5000)
+      
+      
+    });
 
     // setTimeout(()=>
     // {
     //     btn_claroVideo.click();
     // },6000)
+   function clickAutomatico()
+   {
+    
+        setTimeout(()=>
+      {
+      
+        $('#'+(btn_clickeado+1)).click();
+        
+        
+      },5000)
+    }
+
+   clickAutomatico();
+  
+
+  
+
+   
